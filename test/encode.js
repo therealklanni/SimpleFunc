4bf7c7208dd6924126b1b749eb9d5c28da72d985
ajlopez <webmaster@ajlopez.com>
2013-01-06 14:10:11 -0300

    encode simple values, first tests

--- test/encode.js
+++ test/encode.js
@@ -7,3 +7,8 @@ var simplefunc = require('../'),
 assert.ok(simplefunc.encode);
 assert.equal(typeof simplefunc.encode, 'function');
 
+var value = 3;
+assert.equal(simplefunc.encode(value), value);
+
+var value = "foo";
+assert.equal(simplefunc.encode(value), value);
