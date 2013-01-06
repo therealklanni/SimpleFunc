2269f7f85aa500b8bdaf30f0ca744edce657a16c
ajlopez <webmaster@ajlopez.com>
2013-01-06 14:31:43 -0300

    Encode object with function, first test

--- test/encode.js
+++ test/encode.js
@@ -12,3 +12,40 @@ assert.equal(simplefunc.encode(value), value);
 
 var value = "foo";
 assert.equal(simplefunc.encode(value), value);
+
+var value = [1,2,3];
+assert.equal(simplefunc.encode(value), value);
+
+var value = { a: 1, b: 2 };
+assert.equal(simplefunc.encode(value), value);
+
+// encode function
+
+function foo(a, b) {
+    return a+b;
+};
+
+var result = simplefunc.encode(foo);
+assert.ok(result);
+assert.ok(result._fn);
+assert.equal(result._fn.length, 3);
+assert.equal(result._fn[0], 'a');
+assert.equal(result._fn[1], 'b');
+assert.equal(result._fn[2], 'return a+b;');
+
+// encode object with function
+
+var obj = { a: 1, b: 2, foo: function (a, b) { return a+b; } };
+
+var result = simplefunc.encode(obj);
+assert.ok(result);
+assert.ok(result._obj);
+assert.ok(result._obj.a);
+assert.equal(result._obj.a, 1);
+assert.ok(result._obj.b);
+assert.equal(result._obj.b, 2);
+assert.ok(result._fns);
+assert.equal(result._fns.length, 1);
+assert.equal(result._fns[0]._fn[0], 'a');
+assert.equal(result._fns[0]._fn[1], 'b');
+assert.equal(result._fns[0]._fn[2], 'return a+b;');
