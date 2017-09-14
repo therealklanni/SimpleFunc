f40d75dc9359618a5f46d6e2dd27582048cf3f78
Kevin Lanni <therealklanni@gmail.com>
2017-09-14 12:43:11 -0700

    Update to ES6

--- test.js
+++ test.js
@@ -7,48 +7,50 @@ var spawn = require('child_process').spawn;
 var path = require('path');
 
 function runTest(test, cb) {
-	var proc = spawn(process.execPath, [path.join('test', test)], {env: {NODE_PATH: __dirname}});
-	proc.stdout.setEncoding('utf8');
-	proc.stderr.setEncoding('utf8');
+  var proc = spawn(process.execPath, [path.join('test', test)], {env: {NODE_PATH: __dirname}});
+  proc.stdout.setEncoding('utf8');
+  proc.stderr.setEncoding('utf8');
 
-	var stdout = '', stderr = '';
-	proc.stdout.on('data', function(data) {
-		stdout += data;
-	});
-	proc.stderr.on('data', function(data) {
-		stderr += data;
-	});
-	proc.stdin.end();
+  var stdout = '', stderr = '';
+  proc.stdout.on('data', function(data) {
+    stdout += data;
+  });
+  proc.stderr.on('data', function(data) {
+    stderr += data;
+  });
+  proc.stdin.end();
 
-	proc.on('exit', function(code) {
-		if ((stdout !== 'pass\n' && stdout !== '') || stderr !== '') {
-			return cb(new Error(
-					'Test `'+ test+ '` failed.\n'+
-					'code: '+ code+ '\n'+
-					'stderr: '+ stderr+ '\n'+
-					'stdout: '+ stdout));
-		}
-		console.log(test+ ': '+ 'pass');
-		cb();
-	});
+  proc.on('exit', function(code) {
+    if ((stdout !== 'pass\n' && stdout !== '') || stderr !== '') {
+      return cb(new Error(
+          'Test `'+ test+ '` failed.\n'+
+          'code: '+ code+ '\n'+
+          'stderr: '+ stderr+ '\n'+
+          'stdout: '+ stdout));
+    }
+    console.log(test+ ': '+ 'pass');
+    cb();
+  });
 }
 
 var cb = function(err) {
-	if (err) {
-		console.error(String(err));
-		process.exit(1);
-	}
+  if (err) {
+    console.error(String(err));
+    process.exit(1);
+  }
 };
+
 fs.readdirSync('./test').reverse().forEach(function(file) {
     var stats = fs.lstatSync(path.join('./test',file));
     if (!stats.isFile())
-		return;
-	
-	cb = new function(cb, file) {
-		return function(err) {
-			if (err) return cb(err);
-			runTest(file, cb);
-		};
-	}(cb, file);
+    return;
+
+  cb = new function(cb, file) {
+    return function(err) {
+      if (err) return cb(err);
+      runTest(file, cb);
+    };
+  }(cb, file);
 });
+
 cb();
